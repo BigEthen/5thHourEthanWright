b65db046dab1b9b9becd444171d914275e3e3e95
Ethan Wright <ew126542@student.wpsok.org>
2024-08-22 13:01:44 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,18 @@
+#Name Ethan Wright
+#Class 5 hour
+#assigments HW2
+
+
+print("hellow world")
+
+user = input("what is your name?")
+
+print("hello", user)
+
+print("give me a number")
+x= input (" ")
+
+print("give me another number")
+y= input("")
+sum = int(x) + int(y)
+print(sum)
