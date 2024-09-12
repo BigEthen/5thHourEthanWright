0c69bee062db19c3dbd1d01a88dc3e78e6ec8f4b
Ethan Wright <ew126542@student.wpsok.org>
2024-09-12 13:17:09 -0500

    added HW4

---
+++ HW4.py
@@ -0,0 +1,20 @@
+#name Ethan Wright
+#Class 5 hour
+#assigment HW4
+
+#print Hello World!
+print('Hello Word!')
+#Create a dictionary with 3 keys and a value for each key
+#one of the keys must have a value with a list contaINING
+#three number inside
+dic = {"Dog" "Cate"
+       "Xbox" "PlayStation"
+       "abc": [4,5,6]
+       }
+#print one of the three number by itself
+print(dic[4])
+#Using the update function, add a fouth key to the dictionary
+#and give it value
+dic.update({"updated key" "updated value"})
+#print the entire dictionary
+print("dic")
