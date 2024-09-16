2ad5bd75a1859cae5a93d2a3593a7a2097adfc57
Ethan Wright <ew126542@student.wpsok.org>
2024-09-16 13:18:10 -0500

    added Scenario 1

---
+++ Scenario 1.py
@@ -0,0 +1,48 @@
+#name Ethan Wright
+#class 5 hour
+#assigments Scenario 1
+
+#you are a programmer for a fledgling game  developer. Your team lead
+#has asked you to create a nested dictionary containing five enemy
+#creatures (and their properties) for combat testing. Additionally.
+#the tester are asking for a way to input changes to the enemy's
+#damage for balancing,as well as having it print those changes to
+#confirm they went through
+
+BadGuy = {
+"BadGuy1": {
+    "name":  "batman",
+    "size":  "Big",
+    "dmg":  "92",
+    "Hp":    "100",
+},
+"BadGuy2": {
+    "name": "Joker",
+    "size": "small",
+    "dmg":  "43",
+    "Hp":   "65",
+},
+    "BadGuy3": {
+        "name": "catwoman",
+        "size": "small",
+        "dmg":  "34",
+        "Hp":   "54",
+    },
+"BadGuy4": {
+    "name": "bane",
+    "size": "very big",
+    "dmg":   "85",
+    "HP":    "77",
+},
+    "BadGuy5": {
+        "name": "Harley Quinn",
+        "size": "small",
+        "dmg":  "34",
+        "hp":   "65",
+    }
+}
+
+#it is up to you to decide  what properties
+#are important and the theme of the game
+print("BadGuy")
+
