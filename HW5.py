0bde6de052f7a40145009a7d3d6cec9b91ad9345
Ethan Wright <ew126542@student.wpsok.org>
2024-09-27 13:16:31 -0500

    added HW5

---
+++ HW5.py
@@ -0,0 +1,21 @@
+#Name Ethan Wright
+#class 5 hour
+#assigment HW5
+
+
+print("Hello World!")
+
+
+x = int(input(8))
+y = int(input(5))
+z = int(input(2))
+
+
+if x > y and x > z or x == y > z or x == z > y:print (f"The highest number out of {x}, {y} and {z} is: ", x)
+if y > x and y > z or y == z > x:print (f"The highest number out of {x}, {y} and {z} is: ", y)
+if z > x and z > y:print (f"The highest number out of {x}, {y} and {z} is: ", z)
+if x == y == z: print ("All numbers are equal")
+
+
+
+
