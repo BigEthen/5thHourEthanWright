8e393725f28eff2daae2409f19c185351bb346d5
Ethan Wright <ew126542@student.wpsok.org>
2024-10-24 13:07:43 -0500

    added HW9

---
+++ HW9.py
@@ -0,0 +1,53 @@
+#Name:Ethan Wright
+#Class: 5th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+print("hello World!")
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
+from random import randint
+
+temperature = randint(1,22)
+
+
+if temperature >= 20:
+    print("It's hot")
+else:
+    if temperature >=10:
+        print("It's mild")
+    else:
+        print("It's Cold")
+
+
+print("Thank you")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
