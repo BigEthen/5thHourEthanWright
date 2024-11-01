a6c25feffcca49e7b87f7f5ee358e9b5caf09c5c
Ethan Wright <ew126542@student.wpsok.org>
2024-11-01 13:11:37 -0500

    added HW11

--- HW11.py
+++ HW11.py
@@ -7,23 +7,18 @@
 #is divisible by 3 you say "Fizz" instead of the number,
 #every number divisible by 5 you say "Buzz" instead,
 #and if it's divisible by both you say "FizzBuzz".
-x = 100
-while x > 0:
+x = 1
+while x < 100:
 
     if x % 3 == 0 and x % 5 == 0:
         print("FizzBuzz")
+    elif x % 3 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
     else:
-        if x % 3 == 0:
-            print("fizz")
-        else:
-            if x % 5 == 0:
-                print("buzz")
-            else:
-                print(x)
-
-    x -= 1
-
-
+        print(x)
+    x += 1
 
 #Create a while loop that follows the rules of the game.
 #(HINT: Look back to HW6 on how to see if a number is divisible by another)C
