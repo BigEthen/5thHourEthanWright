8b46db210365291c5f6fffa9607e6744438880e1
Ethan Wright <ew126542@student.wpsok.org>
2025-02-04 13:15:31 -0600

    HW19

---
+++ HW19.py
@@ -0,0 +1,25 @@
+#Name:Ethan Wright
+#Class: 5th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+def hello_world():
+    print("hello world!")
+
+def average(one,two,three):
+    return one+two+three / 3
+print (average(3,3,3))
+
+def fun(b1,b2,b3,b4,b5):
+    return [b1,b2,b3,b4,b5]
+fun = fun("cat","Dog","Goat","mouse","cow")
+print("Goat")
+
+for i in range(1):
+    def f():
+     return i
+#2. Call the functions here and run HW19.
+hello_world()
+average()
+fun()
+#3. Delete all calls from HW15 and run HW19 again.
